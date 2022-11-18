c73ae33435bf506dfcba31137770f0d7aeeb7586
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2022-11-18 12:35:03 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectToken } from 'redux/auth/authSelectors';
-import { ReactComponent as AddIcon } from '../../images/logo.svg';
 import { FaPhoneSquareAlt } from 'react-icons/fa';
 
 import s from './Navigation.module.scss';
@@ -13,8 +12,6 @@ export const Navigation = () => {
       <ul className={s.navList}>
         <li className={s.home}>
           <Link to="/">
-            {/* <AddIcon /> */}
-
             <FaPhoneSquareAlt size={60} fill="white" className={s.icon} />
           </Link>
         </li>
